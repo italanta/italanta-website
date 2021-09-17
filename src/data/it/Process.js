6585c42e11bd6d948b51c097360b6f72badd9164
Ian_Odhiambo <ianodhiambo9@gmail.com>
2021-09-17 11:50:41 +0300

    :art: edited the ui on the programs, teams and contact page

--- src/data/it/Process.js
+++ src/data/it/Process.js
@@ -4,25 +4,25 @@ const ProcessData = [
         id:"ps1",
         icon: "1",
         iconBackground: "#5034fc",
-        title: "General Concept",
+        title: "Discover",
         text:
-          "Part of what Adobe does is advise customers about how to transform, to be more",
+          "We identify talented youth from disadvantaged communities, such as refugees, teenage mothers etc., and use our ELEWA Educational Models to train them on software development.",
       },
       {
         id:"ps2",
         icon: "2",
         iconBackground:"#ef4339",
-        title: "Post Product",
+        title: "Develop",
         text:
-          "Part of what Adobe does is advise customers about how to transform, to be more",
+          "We offer a 2-year Apprenticeship Program, where members work in teams on off-shore assignments from local and global customers while gaining a substantial salary",
       },
       {
         id:"ps3",
         icon: "3",
         iconBackground:"#0abfbc",
-        title: "Design Process",
+        title: "Deploy",
         text:
-          "Part of what Adobe does is advise customers about how to transform, to be more",
+          "This is where we deploy the talent we have developed internally, complemented with seniors and freelancers who are transferring their know how and skills to the younger team members.",
       },
   ]
   export default ProcessData;
